04b77a184fe9c3ebb4497e6a78308fd9ffb175ff
Roman Shtylman <shtylman@gmail.com>
2014-11-08 16:17:01 -0800

    v1.0.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'DESCRIPTION.md'), encoding='utf-8') as f:
 setup(
     name='docket',
 
-    version='1.0.0',
+    version='1.0.1',
 
     description='docker builder with private file support',
     long_description=long_description,
